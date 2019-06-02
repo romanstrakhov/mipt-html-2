8119254ca69dc85475e266c962af78ad328d5733
Roman Strakhov <roman@strakhov.ru>
2019-06-03 01:33:31 +0300

    Evening stop

--- w2/harvest/gallery.js
+++ w2/harvest/gallery.js
@@ -2,8 +2,36 @@ let num = 4; // число картинок в строке
 let gap = 20; // px, расстояние между картинками 
 let width = 200; // px ширина картинки
 
-
 // ширина фрейма = 20*(4+1)+200*4
 let frameWidth = gap * (num + 1) + width * num;
+console.log('frameWidth = ' + frameWidth);
 
-console.log(frameWidth);
+let popupImages = Array.from(document.querySelectorAll('.gallery__products__image'));
+popupImages.forEach( e => {
+  let url = e.getAttribute('src');
+  e.parentElement.addEventListener('click', e => {
+    runPopup(url);
+  });
+});
+
+let numAll = popupImages.length;
+let totalWidth = gap * (numAll + 1) + width * numAll;
+console.log('totalWidth = ' + totalWidth);
+
+// console.info(popupImages);
+
+document.addEventListener('keyup', e => {
+  if (e.key === 'Escape') closePopup();
+})
+
+function closePopup() {
+  let divPopup = document.querySelector('.popup');
+  divPopup.classList.add('hidden');
+}
+
+function runPopup(image) {
+  let divPopup = document.querySelector('.popup');
+  divPopup.querySelector('img').setAttribute('src',image);
+  divPopup.classList.remove('hidden');
+
+}
