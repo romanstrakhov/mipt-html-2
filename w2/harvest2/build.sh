23e0dfdfee33772335143362a3af99e20761a2a6
Roman Strakhov <roman@strakhov.ru>
2019-08-04 18:08:31 +0300

    Create task bundle

--- w2/harvest2/build.sh
+++ w2/harvest2/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 rm task.zip
-zip -r task.zip * -x images/originals/* task.md ./*.png build.sh products.json images/.DS_Store
+zip -r task.zip * -x images/originals/* task.md ./*.png build.sh products.json images/.DS_Store test.html
 
